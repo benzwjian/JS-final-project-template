fcd3c7821408fb5e55bb1a81de303d59c4e3f16f
benzwjian <zwjian@gmail.com>
2017-08-14 10:00:48 +0800

    0812 update

--- main.js
+++ main.js
@@ -7,26 +7,44 @@ enemyImg.src = "images/slime.gif";
 var towerBtnImg = document.createElement("img");
 towerBtnImg.src = "images/tower-btn.png";
 
+var towerImg = document.createElement("img");
+towerImg.src = "images/tower.png";
+
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
+var FPS = 60;
+
 var enemy = {
 	x: 96,
-	y: 480 - 32
+	y: 480 - 32,
+	speedX: 0,
+	speedY: -64 / FPS,
+	move: function () {
+		this.x += this.speedX;
+		this.y += this.speedY;
+	}
 }
 
 function draw() {
+	enemy.move();
 	ctx.drawImage(bgImg, 0, 0);
 	ctx.drawImage(enemyImg, enemy.x, enemy.y);
 	ctx.drawImage(towerBtnImg, 640 - 64, 480 - 64, 64, 64);
-	if (isBuilding) ctx.drawImage(towerBtnImg, cursor.x, cursor.y, 64, 64);
+	if (isBuilding) ctx.drawImage(towerImg, cursor.x, cursor.y, 32, 32);
+	ctx.drawImage(towerImg, tower.x, tower.y, 32, 32);
 }
 
-setInterval(draw, 16);
+setInterval(draw, 1000 / FPS);
 
 var cursor = {
 	x: 0,
 	y: 0
+}
+
+var tower = {
+	x: -64,
+	y: -64
 }
 
 var isBuilding = false;
@@ -38,8 +56,10 @@ $('#game-canvas').on('mousemove', function (event) {
 })
 .on('click', function (event) {
 	if (cursor.x > 640 - 64 && cursor.y > 480 - 64) {
-		isBuilding = !isBuilding;
+		isBuilding = true;
 	}  else {
+		tower.x = Math.floor(cursor.x / 32) * 32;
+		tower.y = Math.floor(cursor.y / 32) * 32;
 		isBuilding = false;
 	}
 });
